d591bb7db5a6e25267dc5f98cf565e95d2f989eb
EstebanP <epinczinger@gmail.com>
2021-04-30 19:57:12 -0300

    add index method on accesories

--- app/controllers/accesories_controller.rb
+++ app/controllers/accesories_controller.rb
@@ -1,2 +1,11 @@
 class AccesoriesController < ApplicationController
+  before_action :authenticate_and_set_user
+
+  def index
+    @accesories = current_user.accesories
+
+    respond_to do |format|
+      format.json { render json: @accesories }
+    end
+  end
 end
