38e9fbc25a1bbfd093761d4e466753aedb0dec00
EstebanP <epinczinger@gmail.com>
2021-05-03 15:32:17 -0300

    validate email on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :surfboards, through: :favourites, source: :favouriteable, source_type: 'Surfboard'
   has_many :accesories, through: :favourites, source: :favouriteable, source_type: 'Accesory'
   has_secure_password
+  validates :email, uniqueness: true
 end
