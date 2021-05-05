43de9024c05e2851580ef1677a2fbde939f26284
EstebanP <epinczinger@gmail.com>
2021-05-05 14:14:32 -0300

    modify create and destroy fav controllers

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -6,18 +6,22 @@ class FavouritesController < ApplicationController
     @surfboards = current_user.surfboards
     @accesories = current_user.accesories
     respond_to do |format|
-      format.json { render json: @accesories.to_json + @surfboards.to_json }
+      format.json { render json: @current_user.to_json(:include => [:accesories, :surfboards]) }
     end
   end
 
   def create
-    @favourite = current_user.favourites.new(favouriteable_id: params[:id], favouriteable_type: params[:type])
+    @user = current_user
+    @favourite = @user.favourites.new(favouriteable_id: params[:id], favouriteable_type: params[:type])
     @favourite.save
+    if params[:type] === "Surfboard"
+      @response = @user.surfboards.find(params[:id])
+    elsif params[:type] === "Accesory"
+      @response = @user.accesories.find(params[:id])
+    end
     respond_to do |format|
       format.json do
-        render json: {
-          result: 'Favourite created.'
-        }
+        render json: @response
       end
     end
   rescue StandardError => e
@@ -28,13 +32,17 @@ class FavouritesController < ApplicationController
   end
 
   def destroy
+    @user = current_user
     @favourite = current_user.favourites.find_by(favouriteable_id: params[:id], favouriteable_type: params[:type])
+    if params[:type] === "Surfboard"
+      @response = @user.surfboards.find(params[:id])
+    elsif params[:type] === "Accesory"
+      @response = @user.accesories.find(params[:id])
+    end
     @favourite.destroy
     respond_to do |format|
       format.json do
-        render json: {
-          result: 'Favourite deleted.'
-        }
+        render json: @response
       end
     end
   rescue StandardError => e
@@ -47,6 +55,6 @@ class FavouritesController < ApplicationController
   private
 
   def favourite_params
-    params.require(:favourite).permit(:favouriteable_id, :favouriteable_type)
+    params.require(:favourite).permit(:favouriteable_id, :favouriteable_type, :id, :type)
   end
 end
