688edb047022aca5a07d9adc5d65f2fabd49d67b
EstebanP <epinczinger@gmail.com>
2021-05-06 19:30:37 -0300

    add favourite controllers

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -11,50 +11,25 @@ class FavouritesController < ApplicationController
   end
 
   def create
-    @user = current_user
-    @favourite = @user.favourites.new(favouriteable_id: params[:id], favouriteable_type: params[:type])
+    @favourite = current_user.favourites.new(product_id: params[:id])
+
     @favourite.save
-    if params[:type] === "Surfboard"
-      @response = @user.surfboards.find(params[:id])
-    elsif params[:type] === "Accesory"
-      @response = @user.accesories.find(params[:id])
-    end
-    respond_to do |format|
-      format.json do
-        render json: @response
-      end
-    end
-  rescue StandardError => e
-    respond_to do |format|
-      e = { result: 'Error when adding favourite.' }
-      format.json { render json: e }
-    end
+    render json: { result: "Favourite created." }
+  rescue StandardError
+    render json: { result: 'Error when adding favourite.' }
   end
 
   def destroy
-    @user = current_user
-    @favourite = current_user.favourites.find_by(favouriteable_id: params[:id], favouriteable_type: params[:type])
-    if params[:type] === "Surfboard"
-      @response = @user.surfboards.find(params[:id])
-    elsif params[:type] === "Accesory"
-      @response = @user.accesories.find(params[:id])
-    end
-    @favourite.destroy
-    respond_to do |format|
-      format.json do
-        render json: @response
-      end
-    end
-  rescue StandardError => e
-    respond_to do |format|
-      e = { result: 'Error when deleting favourite.' }
-      format.json { render json: e }
-    end
+    @to_destroy = current_user.favourites.find_by(favourite_params)
+    @to_destroy.destroy
+    render json: { result: "Favourite deleted." }
+  rescue StandardError
+    render json: { result: "Error when deleting." }
   end
 
   private
 
   def favourite_params
-    params.require(:favourite).permit(:favouriteable_id, :favouriteable_type, :id, :type)
+    params.require(:favourite).permit(:user_id, :product_id)
   end
 end
--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,7 +1,7 @@
 class Favourite < ApplicationRecord
   belongs_to :user
   belongs_to :product
-  
-  validates_uniqueness_of :property, scope: :user
-  validates_presence_of :user_id, :property_id
+
+  validates_uniqueness_of :product, scope: :user
+  validates_presence_of :user_id, :product_id
 end
