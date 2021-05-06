658abc3ec03bbc2da7a67bef27376c3362356784
EstebanP <epinczinger@gmail.com>
2021-05-06 18:35:51 -0300

    modify seeds, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'products/show'
+  get 'products/index'
+  get 'products/create'
+  get 'products/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   api_guard_routes for: 'users'
-  resources :surfboards
-  resources :accesories
+  resources :products
   resource :favourites, only: [:create, :destroy]
   get '/favourites', to: 'favourites#index'
   
---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get products_show_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get products_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get products_create_url
+    assert_response :success
+  end
+
+  test "should get delete" do
+    get products_delete_url
+    assert_response :success
+  end
+
+end
